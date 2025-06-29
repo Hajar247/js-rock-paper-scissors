f7839f9600238a0dc7b8513ee1d579c6b7b98ec8
Hajar247 <hajaralmutea@gmail.com>
2025-06-28 22:46:56 -0900

    initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/*------------------------- Constants ------------------------*/
+const choice =['rock','paper','scissers']
+
+
+/*------------------------ Variables -----------------------*/
+let msg 
+let playerchoice
+let computerchoice
+
+
+
+/*----------------- Cached Element References ---------------*/
+const rocbtnel = document.querySelector
+('#rock')
+console.log(rocbtnel)
+
+const papertnel = document.querySelector
+('#paper')
+console.log(papertnel)
+
+const scissirs = document.querySelector
+('#scisser')
+console.log(scissirs)
+
+
+
+
+/*--------------------- Functions ------------------------*/
+
+
+
+/*---------------------- Event Listeners ------------------*/
